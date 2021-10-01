524a47d7ae2223092c2f9ebf39c589bdd2a93b8d
Brandmuffin <btruong100381@gmail.com>
2021-09-30 19:06:28 -0700

    added comment

--- lab_0/four_bit_counter_modern/fourBitCounterModern.v
+++ lab_0/four_bit_counter_modern/fourBitCounterModern.v
@@ -1,5 +1,7 @@
 module fourBitCounterModern (clk, rst, a);
- 
+ /*whenever a positive edge signal on the clock is given, we will start the loop at line 6
+ if reset is 1, then the array will be set back to 0000, thus resetting the counter for counting again
+ otherwise the array counter is incremented by 1, which differs from the schematic in that the schematic uses combinational logic (gates) to count*/
     input clk, rst;
     output reg [3:0] a;
  
