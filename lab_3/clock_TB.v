07a9f772713fb4178f76b8bb2ffb0eb4490ea297
Jinbean Park <59791875+JinbeanPark@users.noreply.github.com>
2021-10-26 13:20:25 -0700

    Create clock_TB.v

---
+++ lab_3/clock_TB.v
@@ -0,0 +1,46 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   13:13:08 10/26/2021
+// Design Name:   clock
+// Module Name:   C:/Project/lab3/clock_TB.v
+// Project Name:  lab3
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: clock
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module clock_TB;
+
+	// Outputs
+	wire ;
+
+	// Instantiate the Unit Under Test (UUT)
+	clock uut (
+		.()
+	);
+
+	initial begin
+		// Initialize Inputs
+
+		// Wait 100 ns for global reset to finish
+		#100;
+        
+		// Add stimulus here
+
+	end
+      
+endmodule
+
